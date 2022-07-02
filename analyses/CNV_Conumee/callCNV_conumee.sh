3f8d65f7eaf67cf4479470b1ebd670b16bb3c135
Rachel Moss <moss0134@umn.edu>
2022-07-02 17:03:49 -0500

    Update callCNV scripts & save plots/sample and segment tables per subgroup

--- analyses/CNV_Conumee/callCNV_conumee.sh
+++ analyses/CNV_Conumee/callCNV_conumee.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=12:00:00
 #SBATCH --mail-type=ALL  
 #SBATCH --mail-user=moss0134@umn.edu 
-#SBATCH --job-name=callCNV
+#SBATCH --job-name=callCNV_New_Group4
 #SBATCH --ntasks=1
 #SBATCH --mem=100g
 #SBATCH --tmp=100g
@@ -12,19 +12,25 @@ set -euo pipefail
 CavDir="/home/spectorl/moss0134/projects/research/MBSexDiff/data/Cavalli"
 NewDir="/home/spectorl/moss0134/projects/research/MBSexDiff/data/Newcastle"
 HeidDir="/home/spectorl/moss0134/projects/research/MBSexDiff/data/Hovestadt"
-controlDir = "/home/spectorl/moss0134/projects/research/MBSexDiff/data/control"
-resultsDir = "/home/spectorl/moss0134/projects/research/MBSexDiff/results"
+controlDir="/home/spectorl/moss0134/projects/research/MBSexDiff/data/control"
+resultsDir="/home/spectorl/moss0134/projects/research/MBSexDiff/results"
 
 cd /home/spectorl/moss0134/projects/research/medulloblastoma_MethylationSexDiff/analyses/CNV_Conumee
 
 module load R/4.0.4
 
-Rscript --vanilla callCNV_conumee.R -a $CavDir/MSet_Cavalli_filt.RDS -b $CavDir/phenoCav.csv -c $controlDir/MSet_control_CBL_GSE134379.RDS -d $controlDir/Control_CBL_GSE134379_pheno_Sample_Sheet.csv -p Cavalli -s SHH -o $resultsDir
+# Rscript --vanilla callCNV_Conumee.R -a $CavDir/MSet_Cavalli_filt.RDS -b $CavDir/phenoCav.csv -c $controlDir/MSet_control_CBL_GSE134379.RDS -d $controlDir/Control_CBL_GSE134379_pheno_Sample_Sheet.csv -p Cavalli -s SHH -o $resultsDir
 
-# Rscript --vanilla callCNV_conumee.R -a $CavDir/MSet_Cavalli_filt.RDS -b $CavDir/phenoCav.csv -c $controlDir/MSet_control_CBL_GSE134379.RDS -d $controlDir/Control_CBL_GSE134379_pheno_Sample_Sheet.csv -p Cavalli -s WNT -o $resultsDir
+#Rscript --vanilla callCNV_Conumee.R -a $CavDir/MSet_Cavalli_filt.RDS -b $CavDir/phenoCav.csv -c $controlDir/MSet_control_CBL_GSE134379.RDS -d $controlDir/Control_CBL_GSE134379_pheno_Sample_Sheet.csv -p Cavalli -s WNT -o $resultsDir
 
-# Rscript --vanilla callCNV_conumee.R -a $CavDir/MSet_Cavalli_filt.RDS -b $CavDir/phenoCav.csv -c $controlDir/MSet_control_CBL_GSE134379.RDS -d $controlDir/Control_CBL_GSE134379_pheno_Sample_Sheet.csv -p Cavalli -s Group3 -o $resultsDir
+#Rscript --vanilla callCNV_Conumee.R -a $CavDir/MSet_Cavalli_filt.RDS -b $CavDir/phenoCav.csv -c $controlDir/MSet_control_CBL_GSE134379.RDS -d $controlDir/Control_CBL_GSE134379_pheno_Sample_Sheet.csv -p Cavalli -s Group3 -o $resultsDir
 
-# Rscript --vanilla callCNV_conumee.R -a $CavDir/MSet_Cavalli_filt.RDS -b $CavDir/phenoCav.csv -c $controlDir/MSet_control_CBL_GSE134379.RDS -d $controlDir/Control_CBL_GSE134379_pheno_Sample_Sheet.csv -p Cavalli -s Group4 -o $resultsDir
+# Rscript --vanilla callCNV_Conumee.R -a $CavDir/MSet_Cavalli_filt.RDS -b $CavDir/phenoCav.csv -c $controlDir/MSet_control_CBL_GSE134379.RDS -d $controlDir/Control_CBL_GSE134379_pheno_Sample_Sheet.csv -p Cavalli -s Group4 -o $resultsDir
 
-# Rscript --vanilla callCNV_conumee.R -a $NewDir/GRSet_Newcastle_GSE93646_filtMBonly.RDS -f $NewDir/Newcastle_pheno_v3.csv -p Newcastle -n 500 -o $NewDir
+# Rscript --vanilla callCNV_Conumee.R -a $NewDir/MSet_Newcastle_GSE93646_filt.RDS -b $NewDir/Newcastle_pheno_v3.csv -c $controlDir/MSet_control_CBL_GSE134379.RDS -d $controlDir/Control_CBL_GSE134379_pheno_Sample_Sheet.csv -p Newcastle -s SHH -o $resultsDir
+
+#Rscript --vanilla callCNV_Conumee.R -a $NewDir/MSet_Newcastle_GSE93646_filt.RDS -b $NewDir/Newcastle_pheno_v3.csv -c $controlDir/MSet_control_CBL_GSE134379.RDS -d $controlDir/Control_CBL_GSE134379_pheno_Sample_Sheet.csv -p Newcastle -s WNT -o $resultsDir
+
+# Rscript --vanilla callCNV_Conumee.R -a $NewDir/MSet_Newcastle_GSE93646_filt.RDS -b $NewDir/Newcastle_pheno_v3.csv -c $controlDir/MSet_control_CBL_GSE134379.RDS -d $controlDir/Control_CBL_GSE134379_pheno_Sample_Sheet.csv -p Newcastle -s Group3 -o $resultsDir
+
+Rscript --vanilla callCNV_Conumee.R -a $NewDir/MSet_Newcastle_GSE93646_filt.RDS -b $NewDir/Newcastle_pheno_v3.csv -c $controlDir/MSet_control_CBL_GSE134379.RDS -d $controlDir/Control_CBL_GSE134379_pheno_Sample_Sheet.csv -p Newcastle -s Group4 -o $resultsDir
